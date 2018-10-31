ed6bb0f7eb3a9bddc867006d2422cab85a696d9c
vm15 <39962840+vm15@users.noreply.github.com>
2018-11-01 00:00:35 +0530

    Add files via upload

---
+++ GCI.py
@@ -0,0 +1,7 @@
+a= "GCI is great"
+for i in range(1,11):
+	i=a
+	print(i)
+a=input("Enter you name : ")
+b=a[:: -1]
+print("Hello",a,",please to meet you! Did you know that your name backwards is",b)	
